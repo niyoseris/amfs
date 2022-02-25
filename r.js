7ebb1253f094c240ec9c5ea712a2c96ea4f509ea
niyotreus <ni.choban@gmail.com>
2022-02-25 11:55:08 +0200

    neler neler

--- r.js
+++ r.js
@@ -1,8 +1,23 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+const utf8 = require("utf8");
+
+
+app.get("/", (req, res) => {
+    res.header("Content-Type", "text/html; charset=utf-8");
+    res.end(don);
+})
+
+
+
 let haberci = require('rss-parser');
 
 let striptags = require('striptags');
 
 let h = new haberci();
+
+let don = '';
 
 rssList = ["https://www.kibrisgazetesi.com/rss", "https://www.yeniduzen.com/rss", "https://cyprus-mail.com/feed/", "http://www.detaykibris.com/rss"];
 
@@ -16,16 +31,22 @@ function bak(site){
         console.log(haber.title);
 
         haber.items.forEach(item => {
-            console.log(item.title + "\n" + item.link + "\n" + temizle(item.content) + "\n");
+            yeni = "<br><br><br>" + item.title + "<br>" + linkle(item.link) + "<br>" + temizle(item.content) + "<br>";
+            don = don + yeni;
         })
     })();
 }
 
 
 function linkle(link){
-    return "<a href=\"" + link + "\"></a>";
+    return "<a href=\"" + link + "\">" + link + "</a>";
 }
 
 function temizle(html){
     return striptags(html,'' , " ").trim();
 }
+
+
+app.listen(PORT, () =>{
+    console.log("helüüü");
+})
