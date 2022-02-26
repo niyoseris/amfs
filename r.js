d663336107bd63a776fc4e79b5963bf06928c130
niyotreus <ni.choban@gmail.com>
2022-02-26 18:20:41 +0200

    bişeyler yaptık

--- r.js
+++ r.js
@@ -7,34 +7,19 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 
+feedz = {
+    "Yeni Düzen": "https://www.yeniduzen.com/rss",
+    "Kıbrıs Gazetesi": "https://www.kibrisgazetesi.com/rss",
+    "Cyprus-Mail": "https://cyprus-mail.com/rss",
+    "Detay Kıbrıs":"http://www.detaykibris.com/rss"
+};
 
 let h = new haberci();
 
-let don = '';
-
-/*rssList = ["https://www.kibrisgazetesi.com/rss", "https://www.yeniduzen.com/rss", "https://cyprus-mail.com/feed/", "http://www.detaykibris.com/rss"];
-
-rssList =  ["https://www.yeniduzen.com/rss"];
-for (r of rssList){
-    bak(r);
-
+for(ll in feedz){
+    console.log(ll);
 }
 
-*/
-
-bak();
-
-function bak(){
-    (async () => {
-        let haber = await h.parseURL("https://www.yeniduzen.com/rss");
-        console.log(haber.title);
-
-        haber.items.forEach(item => {
-            yeni = "<br><br><br>" + item.title + "<br>" + linkle(item.link) + "<br>" + temizle(item.content) + "<br>";
-            don = don + yeni;
-        })
-    })();
-}
 
 
 function linkle(link){
@@ -45,13 +30,52 @@ function temizle(html){
     return striptags(html,'' , " ").trim();
 }
 
+function cevrele(kaynak){
+    sonu = "<center><a href=\'" + kaynak + "\'>" + kaynak + "</a></center><br><br>";
+    return sonu;
+}
+
+
 
 app.listen(PORT, () =>{
     console.log("helüüü " + PORT);
 })
 
 
-app.get("/", (req, res) => {
+app.get('/:kim', (req, res) => {
+    var kim = feedz[req.params.kim];
     res.header("Content-Type", "text/html; charset=utf-8");
-    res.end(don);
+    console.log(kim);
+
+    (async () => {
+
+
+        don = '';
+        try{
+        let haber = await h.parseURL(kim);
+        console.log(haber.title);
+
+        haber.items.forEach(item => {
+            yeni = "<br><br>" +  item.title + "<br>" + linkle(item.link) + "<br>" + temizle(item.content) + "<br>";
+            don = don + yeni;
+        })
+
+        res.end(don);
+
+    } catch(err){
+        console.log(err);
+    }
+
+
+    })();
+
+})
+
+app.get('/', (req, res) => {
+    res.header("Content-Type", "text/html; charset=utf-8");
+    dur = '';
+    for(s in feedz){
+        dur = dur + cevrele(s);
+    }
+    res.end(dur);
 })
